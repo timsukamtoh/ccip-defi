d868eabf45309bc44932b80da1135f4cd55354db
Tim Sukamtoh <timtoh168@gmail.com>
2024-03-02 00:27:00 -0700

    edited contracts to emit approval

--- packages/hardhat/contracts/Borrowing.sol
+++ packages/hardhat/contracts/Borrowing.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import { OwnerIsCreator } from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
 import { Lending } from "./Lending.sol";
 import { MockUSDC } from "./MockUSDC.sol";
@@ -12,30 +14,56 @@ import { MockUSDC } from "./MockUSDC.sol";
 /// @title - A simple messenger contract for sending/receiving messages and tokens across chains.
 /// Pay using LINK tokens
 contract Borrowing is OwnerIsCreator {
-	Lending private _mLending;
+	Lending private _Lending;
+	IERC20 usdc;
+	event ApprovalNeeded(
+		address indexed owner,
+		address indexed spender,
+		uint256 value
+	);
 
 	mapping(address => mapping(address => uint256)) public borrowings; // debt address => (token => amount)
 
 	constructor(address lendingAddress) {
-		_mLending = Lending(lendingAddress);
+		_Lending = Lending(lendingAddress);
 	}
 
 	/// called by frontend AFTER determining that user doesn't exceed collatorization ratio across chains
 	/// AND that there's enough deposits of tokenToBorrow on the current chain;
 	/// transfers amountToBorrow to caller's wallet if successful
 	function borrow(address tokenToBorrow, uint256 amountToBorrow) public {
-		require(borrowings[msg.sender][tokenToBorrow] == 0, "Already borrowing USDC");
-		MockUSDC usdc = MockUSDC(tokenToBorrow);
+		require(
+			borrowings[msg.sender][tokenToBorrow] == 0,
+			"Already borrowing USDC"
+		);
+		require(amountToBorrow > 0, "Must borrow more than 0");
+
+		usdc = MockUSDC(tokenToBorrow);
 
 		borrowings[msg.sender][tokenToBorrow] += amountToBorrow;
 
-		usdc.transfer(address(msg.sender), amountToBorrow);
+		usdc.transferFrom(address(this), msg.sender, amountToBorrow);
 	}
 
 	/// deposits into lendings on the current chain
 	function repay(address tokenToRepay, uint256 amount) public {
-		require(amount == borrowings[msg.sender][tokenToRepay], "Must Repay full amount");
-		_mLending.deposit(tokenToRepay, amount);
+		require(
+			amount == borrowings[msg.sender][tokenToRepay],
+			"Must Repay full amount"
+		);
+		require(amount > 0, "Must repay more than 0");
+
+		// Check if the contract has sufficient allowance
+		if (usdc.allowance(msg.sender, address(this)) < amount) {
+			// Emit event to request approval
+			emit ApprovalNeeded(msg.sender, address(this), amount);
+			// You may also provide instructions on how the user can approve spending
+			revert("Please approve spending in your wallet");
+		}
+
+		// Transfer the tokens and update the lending mapping
+		usdc.transferFrom(msg.sender, address(this), amount);
+		borrowings[msg.sender][tokenToRepay] -= amount;
 	}
 
 	/// returns tokenType amount borrowed
--- packages/hardhat/contracts/Lending.sol
+++ packages/hardhat/contracts/Lending.sol
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import { OwnerIsCreator } from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
 import { MockUSDC } from "./MockUSDC.sol";
 
-contract Lending is OwnerIsCreator {
+contract Lending is ReentrancyGuard, OwnerIsCreator {
 	mapping(address => mapping(address => uint256)) public lendings; // depsoit address => (token => amount)
+
+	IERC20 usdc;
+	event ApprovalNeeded(
+		address indexed owner,
+		address indexed spender,
+		uint256 value
+	);
 
 	constructor() {}
 
@@ -15,9 +25,21 @@ contract Lending is OwnerIsCreator {
 
 	/// deposits token to be used as collatoral to borrow on other chains
 	function deposit(address tokenToLend, uint256 amountToLend) public {
-		MockUSDC usdc = MockUSDC(tokenToLend);
+		require(amountToLend > 0, "Must deposit more than 0");
+
+		usdc = MockUSDC(tokenToLend);
+
+		// Check if the contract has sufficient allowance
+		if (usdc.allowance(msg.sender, address(this)) < amountToLend) {
+			// Emit event to request approval
+			emit ApprovalNeeded(msg.sender, address(this), amountToLend);
+			// You may also provide instructions on how the user can approve spending
+			revert("Please approve spending in your wallet");
+		}
+
+		// Transfer the tokens and update the lending mapping
+		usdc.transferFrom(msg.sender, address(this), amountToLend);
 		lendings[address(msg.sender)][tokenToLend] += amountToLend;
-		usdc.transfer(address(this), amountToLend);
 	}
 
 	/// withdraws up to the collatorization ratio across chains
@@ -25,11 +47,14 @@ contract Lending is OwnerIsCreator {
 		address tokenToWithdraw,
 		uint256 amountToWithdraw
 	) public {
-		MockUSDC usdc = MockUSDC(tokenToWithdraw);
+		require(
+			amountToWithdraw <= lendings[address(msg.sender)][tokenToWithdraw],
+			"Withdrawing too much"
+		);
 
+		usdc = MockUSDC(tokenToWithdraw);
 		lendings[address(msg.sender)][tokenToWithdraw] -= amountToWithdraw;
-
-		usdc.transfer(address(msg.sender), amountToWithdraw);
+		usdc.transferFrom(address(this), msg.sender, amountToWithdraw);
 	}
 
 	/// returns tokenType amount borrowed
