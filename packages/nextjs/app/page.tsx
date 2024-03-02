69c1f410e27646797533f06e78f918c3406c1d2a
Tim Sukamtoh <timtoh168@gmail.com>
2024-03-01 23:31:22 -0700

    implemented deposits and borrowed balances

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,32 +1,54 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NextPage } from "next";
-// import { useScaffoldContractRead } from "~~/hooks/scaffold-eth";
+import { useAccount } from "wagmi";
 import { BorrowForm } from "~~/components/forms/BorrowForm";
 import { DepositForm } from "~~/components/forms/DepositForm";
 import { RepayForm } from "~~/components/forms/RepayForm";
 import { WithdrawForm } from "~~/components/forms/WithdrawForm";
+import { useDeployedContractInfo, useScaffoldContractRead } from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
   // State to track the active tab, with 'supply' as the initial state
+  const { data: mockUSDC } = useDeployedContractInfo("MockUSDC");
   const [activeTab, setActiveTab] = useState("supply");
-  // const [userDeposits, setUserDeposits] = useState<string>("0");
-  // const [userBorrowing, setUserBorrowing] = useState<string>("0");
+  const { address } = useAccount();
+  const [userDeposits, setUserDeposits] = useState<string | bigint>("0");
+  const [userDebt, setUserDebt] = useState<string | bigint>("0");
 
-  // useEffect(() => {
-  //   const { data: totalCounter } = useScaffoldContractRead({
-  //     contractName: "YourContract",
-  //     functionName: "userGreetingCounter",
-  //     args: ["0xd8da6bf26964af9d7eed9e03e53415d37aa96045"],
-  //   });
-  //   const { data: totalCounter } = useScaffoldContractRead({
-  //     contractName: "YourContract",
-  //     functionName: "userGreetingCounter",
-  //     args: ["0xd8da6bf26964af9d7eed9e03e53415d37aa96045"],
-  //   });
+  const { data: lendingAmount } = useScaffoldContractRead({
+    contractName: "Lending",
+    functionName: "lendings",
+    args: [address, mockUSDC?.address],
+  });
+  const { data: debtAmount } = useScaffoldContractRead({
+    contractName: "Borrowing",
+    functionName: "borrowings",
+    args: [address, mockUSDC?.address],
+  });
 
-  // }, [userDeposits, userBorrowing]);
+  useEffect(() => {
+    // Check if lendingAmount is not undefined before updating state
+    if (lendingAmount !== undefined) {
+      setUserDeposits(lendingAmount);
+    }
+  }, [address, mockUSDC?.address, lendingAmount]);
+
+  useEffect(() => {
+    // Check if debtAmount is not undefined before updating state
+    if (debtAmount !== undefined) {
+      setUserDebt(debtAmount);
+    }
+  }, [address, mockUSDC?.address, debtAmount]);
+
+  // const updateUserDeposits = (newDeposits) => {
+  //   setUserDeposits(newDeposits);
+  // };
+
+  // const updateUserDebt = (newDebt) => {
+  //   setUserDebt(newDebt);
+  // };
 
   return (
     <div className="absolute inset-0 flex flex-col justify-center items-center m-3 p-9 gap-6">
@@ -55,13 +77,16 @@ const Home: NextPage = () => {
         {activeTab === "supply" ? (
           // Supply content
           <div className="flex flex-col justify-between gap-2 mb-3">
-            <select className="w-30 max-w-xs mx-auto text-xl font-normal text-black bg-lime-400 rounded p-2 shadow font-poppins flex justify-center">
-              <option>USDC</option>
-            </select>
+            <div className="flex items-center justify-center gap-2 mb-3">
+              <strong>Deposited {String(userDeposits)}</strong>
+              <select className="w-30 max-w-xs text-xl font-normal text-black bg-lime-400 rounded p-2 shadow font-poppins">
+                <option>USDC</option>
+              </select>
+            </div>
             <div></div>
             <div className="w-50 max-w-md mx-auto flex flex-row justify-between gap-4 mb-6">
               <DepositForm />
-              <WithdrawForm />
+              {Number(userDeposits) >= 0 ? <WithdrawForm /> : <></>}
             </div>
             <div className="w-80 max-w-md mx-auto mb-6">
               <div className="text-lg font-normal text-black mb-3 font-poppins">Supply rates</div>
@@ -82,23 +107,25 @@ const Home: NextPage = () => {
         ) : (
           // Borrow content
           <div className="flex flex-col justify-between gap-2 mb-3">
-            <select className="w-30 max-w-xs mx-auto text-xl font-normal text-black bg-lime-400 rounded p-2 shadow font-poppins flex justify-center">
-              <option>USDC</option>
-            </select>
+            <div className="flex items-center justify-center gap-2 mb-3">
+              <strong>Borrowed {String(userDebt)}</strong>
+              <select className="w-30 max-w-xs text-xl font-normal text-black bg-lime-400 rounded p-2 shadow font-poppins">
+                <option>USDC</option>
+              </select>
+            </div>
             <div></div>
             <div className="w-50 max-w-md mx-auto flex flex-row justify-between gap-4 mb-6">
-              <BorrowForm />
-              <RepayForm />
+              {Number(userDebt) == 0 ? <BorrowForm /> : <RepayForm />}
             </div>
             <div className="w-80 max-w-md mx-auto mb-6">
-              <div className="text-lg font-normal text-black mb-3 font-poppins">Borrow rates</div>
+              <div className="text-lg font-normal text-black mb-3 font-poppins">Supply rates</div>
               <div className="flex justify-between">
-                <div className="text-md font-normal text-neutral-500 font-poppins">Borrow APR</div>
+                <div className="text-md font-normal text-neutral-500 font-poppins">Borrowing APR</div>
                 <div className="text-md font-normal text-black font-poppins">10.00%</div>
               </div>
               <div className="flex justify-between">
                 <div className="text-md font-normal text-neutral-500 font-poppins">Distribution APR</div>
-                <div className="text-md font-normal text-black font-poppins">1.90%</div>
+                <div className="text-md font-normal text-black font-poppins">2.00%</div>
               </div>
               <div className="flex justify-between">
                 <div className="text-md font-normal text-neutral-500 font-poppins">Liquidity Pool</div>
